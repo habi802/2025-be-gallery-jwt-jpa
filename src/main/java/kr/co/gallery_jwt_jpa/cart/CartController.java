ba88d88f80aa604bd23884c2df03bb97b06f28af
habi802 <shb8002@gmail.com>
2025-08-04 11:38:17 +0900

    cart 컨트롤러 세션에서 인증 처리로 수정

--- src/main/java/kr/co/gallery_jwt_jpa/cart/CartController.java
+++ src/main/java/kr/co/gallery_jwt_jpa/cart/CartController.java
@@ -5,10 +5,12 @@ import kr.co.gallery_jwt_jpa.account.etc.AccountConstants;
 import kr.co.gallery_jwt_jpa.cart.model.CartDeleteReq;
 import kr.co.gallery_jwt_jpa.cart.model.CartGetRes;
 import kr.co.gallery_jwt_jpa.cart.model.CartPostReq;
+import kr.co.gallery_jwt_jpa.config.model.UserPrincipal;
 import kr.co.gallery_jwt_jpa.config.util.HttpUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,33 +23,37 @@ public class CartController {
     private final CartService cartService;
 
     @PostMapping
-    public ResponseEntity<?> save(@RequestBody CartPostReq req, HttpServletRequest httpReq) {
+    public ResponseEntity<?> save(@RequestBody CartPostReq req, @AuthenticationPrincipal UserPrincipal userPrincipal) {
         log.info("req: {}", req);
-        int logginedMemberId = (int) HttpUtils.getSessionValue(httpReq, AccountConstants.MEMBER_ID_NAME);
-        req.setMemberId(logginedMemberId);
+        //int logginedMemberId = (int) HttpUtils.getSessionValue(httpReq, AccountConstants.MEMBER_ID_NAME);
+        //req.setMemberId(logginedMemberId);
+        req.setMemberId(userPrincipal.getMemberId());
         int result = cartService.save(req);
         return ResponseEntity.ok(result);
     }
 
     @GetMapping
-    public ResponseEntity<?> findAll(HttpServletRequest httpReq) {
-        int logginedMemberId = (int) HttpUtils.getSessionValue(httpReq, AccountConstants.MEMBER_ID_NAME);
+    public ResponseEntity<?> findAll(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        //int logginedMemberId = (int) HttpUtils.getSessionValue(httpReq, AccountConstants.MEMBER_ID_NAME);
+        int logginedMemberId = userPrincipal.getMemberId();
         List<CartGetRes> result = cartService.findAll(logginedMemberId);
         log.info("result: {}", result);
         return ResponseEntity.ok(result);
     }
 
     @DeleteMapping("/{cartId}")
-    public ResponseEntity<?> deleteMemberItem(@PathVariable int cartId, HttpServletRequest httpReq) {
-        int logginedMemberId = (int) HttpUtils.getSessionValue(httpReq, AccountConstants.MEMBER_ID_NAME);
+    public ResponseEntity<?> deleteMemberItem(@PathVariable int cartId, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        //int logginedMemberId = (int) HttpUtils.getSessionValue(httpReq, AccountConstants.MEMBER_ID_NAME);
+        int logginedMemberId = userPrincipal.getMemberId();
         CartDeleteReq req = new CartDeleteReq(cartId, logginedMemberId);
         int result = cartService.removeItem(req);
         return ResponseEntity.ok(result);
     }
 
     @DeleteMapping
-    public ResponseEntity<?> deleteMemberCart(HttpServletRequest httpReq) {
-        int logginedMemberId = (int) HttpUtils.getSessionValue(httpReq, AccountConstants.MEMBER_ID_NAME);
+    public ResponseEntity<?> deleteMemberCart(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        //int logginedMemberId = (int) HttpUtils.getSessionValue(httpReq, AccountConstants.MEMBER_ID_NAME);
+        int logginedMemberId = userPrincipal.getMemberId();
         int result = cartService.removeCart(logginedMemberId);
         return ResponseEntity.ok(result);
     }
