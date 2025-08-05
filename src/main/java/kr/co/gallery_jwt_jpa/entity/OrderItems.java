a7249da571d93f93dea8a448e5c93e514cfbee8f
habi802 <shb8002@gmail.com>
2025-08-05 14:51:48 +0900

    order_items 엔터티 추가

---
+++ src/main/java/kr/co/gallery_jwt_jpa/entity/OrderItems.java
@@ -0,0 +1,24 @@
+package kr.co.gallery_jwt_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode
+public class OrderItems extends Created {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
+    @JoinColumn(name="order_id", nullable = false)
+    private Orders orders;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id", nullable = false)
+    private Items items;
+}
