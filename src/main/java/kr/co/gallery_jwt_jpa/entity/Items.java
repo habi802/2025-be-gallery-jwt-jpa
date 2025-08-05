3f9862db95d2fee3048950c589569c9970aa0a26
habi802 <shb8002@gmail.com>
2025-08-05 13:17:03 +0900

    items 엔터티 추가

---
+++ src/main/java/kr/co/gallery_jwt_jpa/entity/Items.java
@@ -0,0 +1,28 @@
+package kr.co.gallery_jwt_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode
+public class Items extends Created {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String imgPath;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int discountPer;
+}
